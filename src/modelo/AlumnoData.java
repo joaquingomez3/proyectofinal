f2d1f9325bf81bd54570d9a0ee5a97932efd8db9
kureplastic <72420325+kureplastic@users.noreply.github.com>
2020-10-22 16:49:55 -0300

    Algunos cambios minimos

--- src/modelo/AlumnoData.java
+++ src/modelo/AlumnoData.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package modelo;
 
 import entidades.Alumno;
@@ -11,123 +16,124 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JOptionPane;
 
-
+/**
+ *
+ * @author Fran Gonzalez
+ */
 public class AlumnoData {
-    
+
     private Connection con;
-    
-    public AlumnoData(Conexion conexion){
+
+    public AlumnoData(Conexion conexion) {
         con = conexion.getConnection();
     }
-    
-    public void guardarAlumno(Alumno alumno){
-        
-        String sql= "INSERT INTO `alumno` (`nombre`,`apellido`, `fechaNac`, `activo`) "
+
+    public void guardarAlumno(Alumno alumno) {
+
+        String sql = "INSERT INTO `alumno` (`nombre`,`apellido`, `fechaNac`, `activo`) "
                 + "VALUES (?, ?, ?, ?)";
-        
-        try{
+
+        try {
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-            ps.setString(1,alumno.getNombre() );
+            ps.setString(1, alumno.getNombre());
             ps.setString(2, alumno.getApellido());
             ps.setDate(3, Date.valueOf(alumno.getFechaNac()));
             ps.setBoolean(4, alumno.isActivo());
-        
+
             ps.executeUpdate();
-            
+
             ResultSet rs = ps.getGeneratedKeys();
-            
+
             if (rs.next()) {
                 alumno.setIdAlumno(rs.getInt(1));
-                JOptionPane.showMessageDialog(null,"El alumno ha sido cargado");
+                JOptionPane.showMessageDialog(null, "El alumno ha sido cargado");
             } else {
-                JOptionPane.showMessageDialog(null,"No puedo obtener id");
+                JOptionPane.showMessageDialog(null, "No puedo obtener id");
             }
-       
-            
+
             rs.close();
             ps.close();
-        //con.close();
-            
-        } catch (SQLException e){
+            //con.close();
+
+        } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Error al guardar Alumno en DB");
         }
-        
-        
+
     }
-    
-    public void eliminarAlumno(Alumno alumno){
-        String sql="DELETE FROM `alumno` WHERE idAlumno = ?";
-        try{
-        
-        PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-        ps.setInt(1, alumno.getIdAlumno());
-        
-        ps.executeUpdate();
-        
-              
-        ps.close();
-        }catch(SQLException e){
-        
-            JOptionPane.showMessageDialog(null,"Error al eliminar Alumno");
-        } 
+
+    public void eliminarAlumno(Alumno alumno) {
+        String sql = "DELETE FROM `alumno` WHERE idAlumno = ?";
+        try {
+
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, alumno.getIdAlumno());
+
+            ps.executeUpdate();
+
+            ps.close();
+        } catch (SQLException e) {
+
+            JOptionPane.showMessageDialog(null, "Error al eliminar Alumno");
+        }
     }
-    
-    public Alumno buscarAlumno(int idAlumno){
+
+    public Alumno buscarAlumno(int idAlumno) {
         Alumno alumno = null;
         String sql = "SELECT * FROM alumno WHERE idAlumno = ?";
-                
-        try{
+
+        try {
             PreparedStatement ps = con.prepareStatement(sql);
-                    ps.setInt(1, idAlumno);
-                    
-                    ResultSet rs = ps.executeQuery();
-                    
-                    if(rs.next()){
-                        JOptionPane.showMessageDialog(null,"ALUMNO ENCONTRADO");
-                        alumno = new Alumno();
-                        alumno.setIdAlumno(rs.getInt("idAlumno"));
-                        alumno.setNombre(rs.getString("nombre"));
-                        alumno.setApellido(rs.getString("apellido"));
-                        alumno.setFechaNac(rs.getDate("fechaNac").toLocalDate());
-                        alumno.setActivo(rs.getBoolean("activo"));
-                    }
-                    ps.close();
-            
-        }catch (SQLException ex){
+            ps.setInt(1, idAlumno);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                JOptionPane.showMessageDialog(null, "ALUMNO ENCONTRADO");
+                alumno = new Alumno();
+                alumno.setIdAlumno(rs.getInt("idAlumno"));
+                alumno.setNombre(rs.getString("nombre"));
+                alumno.setApellido(rs.getString("apellido"));
+                alumno.setFechaNac(rs.getDate("fechaNac").toLocalDate());
+                alumno.setActivo(rs.getBoolean("activo"));
+            }
+            else{JOptionPane.showMessageDialog(null, "NO SE ENCONTRO EL ALUMNO CON DICHO ID");}
+            ps.close();
+            rs.close();
+
+        } catch (SQLException ex) {
             JOptionPane.showMessageDialog(null, "NO SE ENCONTRO EL ALUMNO CON DICHO ID");
         }
-        
+
         return alumno;
     }
-    
-    
-    public List<Alumno> obtenerAlumno(){
+
+    public List<Alumno> obtenerAlumnos() {
         Alumno alumno = null;
-        List <Alumno> alumnos = new ArrayList<>();
+        List<Alumno> alumnos = new ArrayList<>();
         String sql = "SELECT * FROM alumno";
-        
-        try{
+
+        try {
             PreparedStatement ps = con.prepareStatement(sql);
-                                        
-                    ResultSet rs = ps.executeQuery();
-                    
-                    JOptionPane.showMessageDialog(null,"ALUMNOS ENCONTRADOS:");
-                    while(rs.next()){
-                        alumno = new Alumno();
-                        alumno.setIdAlumno(rs.getInt("idAlumno"));
-                        alumno.setNombre(rs.getString("nombre"));
-                        alumno.setApellido(rs.getString("apellido"));
-                        alumno.setFechaNac(rs.getDate("fechaNac").toLocalDate());
-                        alumno.setActivo(rs.getBoolean("activo"));
-                        alumnos.add(alumno);
-                    }
-                    ps.close();
-            
-        }catch (SQLException ex){
-            JOptionPane.showMessageDialog(null, "NO SE ENCONTRO EL ALUMNO CON DICHO ID");
+
+            ResultSet rs = ps.executeQuery();
+
+            JOptionPane.showMessageDialog(null, "ALUMNOS ENCONTRADOS:");
+            while (rs.next()) {
+                alumno = new Alumno();
+                alumno.setIdAlumno(rs.getInt("idAlumno"));
+                alumno.setNombre(rs.getString("nombre"));
+                alumno.setApellido(rs.getString("apellido"));
+                alumno.setFechaNac(rs.getDate("fechaNac").toLocalDate());
+                alumno.setActivo(rs.getBoolean("activo"));
+                alumnos.add(alumno);
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR DE TIPO: " + ex);
         }
-        
+
         return alumnos;
     }
-    
+
 }
